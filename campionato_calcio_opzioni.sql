c91e94a3bf4be0fbfd072f212485ead7f8b230c3
mmattia94 <51045933+mmattia94@users.noreply.github.com>
2019-07-25 00:07:20 +0200

    Update campionato_calcio_opzioni.sql

--- campionato_calcio_opzioni.sql
+++ campionato_calcio_opzioni.sql
@@ -4,8 +4,8 @@ LIMIT 0, 1000
 
 -- Date: 2019-07-24 23:57
 */
-INSERT INTO `` (`id`,`nome`,`code`,`icon`) VALUES (1,'Calendario Partite','calendario','calendar-alt');
-INSERT INTO `` (`id`,`nome`,`code`,`icon`) VALUES (2,'Risultati Giornate di Campionato','risultati','poll');
-INSERT INTO `` (`id`,`nome`,`code`,`icon`) VALUES (3,'Schedina delle Giornate','schedina','meteor');
-INSERT INTO `` (`id`,`nome`,`code`,`icon`) VALUES (4,'Classifica Finale Campionato','classifica','list-ol');
-INSERT INTO `` (`id`,`nome`,`code`,`icon`) VALUES (5,'Statistiche Squadre','squadra','address-card');
+INSERT INTO Campionato_Calcio_opzioni (`id`,`nome`,`code`,`icon`) VALUES (1,'Calendario Partite','calendario','calendar-alt');
+INSERT INTO Campionato_Calcio_opzioni (`id`,`nome`,`code`,`icon`) VALUES (2,'Risultati Giornate di Campionato','risultati','poll');
+INSERT INTO Campionato_Calcio_opzioni (`id`,`nome`,`code`,`icon`) VALUES (3,'Schedina delle Giornate','schedina','meteor');
+INSERT INTO Campionato_Calcio_opzioni (`id`,`nome`,`code`,`icon`) VALUES (4,'Classifica Finale Campionato','classifica','list-ol');
+INSERT INTO Campionato_Calcio_opzioni (`id`,`nome`,`code`,`icon`) VALUES (5,'Statistiche Squadre','squadra','address-card');
