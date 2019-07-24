ada69c9339f7e8df8802e9f32cb2e3ab36eac1bb
mmattia94 <51045933+mmattia94@users.noreply.github.com>
2019-07-25 00:04:57 +0200

    Add files via upload

---
+++ campionato_calcio_opzioni.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM esame.campionato_calcio_opzioni
+LIMIT 0, 1000
+
+-- Date: 2019-07-24 23:57
+*/
+INSERT INTO `` (`id`,`nome`,`code`,`icon`) VALUES (1,'Calendario Partite','calendario','calendar-alt');
+INSERT INTO `` (`id`,`nome`,`code`,`icon`) VALUES (2,'Risultati Giornate di Campionato','risultati','poll');
+INSERT INTO `` (`id`,`nome`,`code`,`icon`) VALUES (3,'Schedina delle Giornate','schedina','meteor');
+INSERT INTO `` (`id`,`nome`,`code`,`icon`) VALUES (4,'Classifica Finale Campionato','classifica','list-ol');
+INSERT INTO `` (`id`,`nome`,`code`,`icon`) VALUES (5,'Statistiche Squadre','squadra','address-card');
